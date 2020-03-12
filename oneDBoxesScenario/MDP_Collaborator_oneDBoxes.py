b20e9d9ef9d0d4bf63917579f0c5faf7ce8478db
carreirabruno <bruno.carreira97@hotmail.com>
2020-03-12 16:40:58 +0000

    oneDBoxes - update do MDP

--- oneDBoxesScenario/MDP_Collaborator_oneDBoxes.py
+++ oneDBoxesScenario/MDP_Collaborator_oneDBoxes.py
@@ -1,184 +1,147 @@
 import numpy as np
 import random
-from copy import deepcopy
-
+import copy
 
 class State:
 
-    def __init__(self, grid, shappy_x_pos):
-        self.grid = grid
-        self.shappy_x_pos = shappy_x_pos
+    def __init__(self, map, shappy_pos):
+        self.map = map
+        self.shappy_pos = shappy_pos
 
     def __eq__(self, other):
-        return isinstance(other, State) and self.grid == other.grid and self.shappy_x_pos == other.shappy_x_pos
+        return isinstance(other, State) and self.map == other.map and self.shappy_pos == other.shappy_pos
 
     def __hash__(self):
-        return hash(str(self.grid) + str(self.shappy_x_pos))
+        return hash(str(self.map) + str(self.shappy_pos))
 
     def __str__(self):
-        return f"State(grid={self.grid}, shappy_x_pos={self.shappy_x_pos})"
+        return f"State(grid={self.map}, shappy_x_pos={self.shappy_pos})"
 
+
+random.seed(42)
 
 # Environment
+WALL = "w"
 BOX = "b"
 COL_SHAPPY = "c"
 NON_COL_SHAPPY = "n"
 EMPTY = "*"
 
-grid = [BOX, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BOX, EMPTY, COL_SHAPPY, EMPTY, BOX]
+map = [WALL, BOX, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BOX, EMPTY, BOX, COL_SHAPPY, EMPTY, BOX, WALL]
 
 # Actions
 LEFT = 0
 STAY = 1
 RIGHT = 2
-
 ACTIONS = [LEFT, STAY, RIGHT]
+n_actions = len(ACTIONS)
 
 pos = -1
-
-for i in range(len(grid)):
-    if grid[i] == "c":
+for i in range(len(map)):
+    if map[i] == "c":
         pos = i
 
-start_state = State(grid=grid, shappy_x_pos=pos)
+start_state = State(map=map, shappy_pos=pos)
 
+gamma = 0.9
+learning_rate = 0.1  # alpha
 
-def act(state, action):
-    def new_shappy_x_pos(state, action):
-        p = deepcopy(state.shappy_x_pos)
-        if action == LEFT:
-            p = max(0, p - 1)
-        elif action == STAY:
-            p = p
-        elif action == RIGHT:
-            p = min(len(grid) - 1, p + 1)
+n_states = len(map)  # * n_actions * 4       #4 é o numero de boxes
 
-        else:
-            raise ValueError(f"Unknown action {action}")
-        return p
-
-    p = new_shappy_x_pos(state, action)
-    grid_item = state.grid[p]
-
-    new_grid = deepcopy(state.grid)
-    if "b" not in grid:
-        is_done = True
-    elif grid_item == BOX:
-        reward = 10
-        is_done = False
-        old = state.shappy_x_pos
-        new_grid[old] = EMPTY
-        new_grid[p] = COL_SHAPPY
-    elif grid_item == EMPTY:
-        reward = -1
-        is_done = False
-        old = state.shappy_x_pos
-        new_grid[old] = EMPTY
-        new_grid[p] = COL_SHAPPY
-    elif grid_item == COL_SHAPPY:
-        reward = -1
-        is_done = False
-    else:
-        raise ValueError(f"Unknown grid item {grid_item}")
-
-    return State(grid=new_grid, shappy_x_pos=p), reward, is_done
-
-
-random.seed(42)  # for reproducibility
-N_STATES = len(grid)
-N_EPISODES = 20000
-
-MAX_EPISODE_STEPS = 100
-
-MIN_ALPHA = 0.02
-
-alphas = np.linspace(1.0, MIN_ALPHA, N_EPISODES)
-gamma = 1.0
-eps = 0.2
-
-q_table = dict()
-
-
-def q(state, action=None):
-    if state not in q_table:
-        q_table[state] = np.zeros(len(ACTIONS))
-
-    if action is None:
-        return q_table[state]
-
-    return q_table[state][action]
-
+#Q_table = np.zeros((n_states, n_actions))
+Q_table = dict()
 
 def choose_action(state):
-    if random.uniform(0, 1) < eps:
-        return random.choice(ACTIONS)
+    if state not in Q_table:
+        Q_table[state] = np.zeros(len(ACTIONS))
+
+    if random.random() < 0.1:  # exploration
+        return random.choices(ACTIONS)
+    else:  # exploitation
+        return np.argmax(Q_table[state])
+
+
+def take_action(state, action):
+    def new_shappy_pos(state, action):
+        pos = copy.deepcopy(state.shappy_pos)
+        if action == LEFT:
+            if state.map[pos - 1] == WALL:  # colidiu com uma wall
+                pos = pos
+            else:
+                pos = max(0, pos - 1)
+        elif action == STAY:
+            pos = pos
+        elif action == RIGHT:
+            if state.map[pos + 1] == WALL:  # colidiu com uma wall
+                pos = pos
+            else:
+                pos = min(len(state.map) - 1, pos + 1)
+        else:
+            raise ValueError(f"Unknown action {action}")
+        return pos
+
+    new_pos = new_shappy_pos(state, action)
+    map_item = state.map[new_pos]
+
+    new_map = copy.deepcopy(state.map)
+    if map_item == BOX:
+        reward = 10
+        old_pos = state.shappy_pos
+        new_map[old_pos] = EMPTY
+        new_map[new_pos] = COL_SHAPPY
+    elif map_item == EMPTY:
+        reward = -1
+        old_pos = state.shappy_pos
+        new_map[old_pos] = EMPTY
+        new_map[new_pos] = COL_SHAPPY
+    elif map_item == COL_SHAPPY:
+        reward = -1
     else:
-        return np.argmax(q(state))
+        raise ValueError(f"Unknown grid item {map_item}")
+
+    return State(map=new_map, shappy_pos=pos), reward
 
 
-#############################################
+def learn(state, action, reward, new_state):
+    # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+    print(state, action, reward, new_state)
+    print(Q_table[state])
+    Q_table(state)[action] = Q_table[state, action] + learning_rate * \
+                             (reward + gamma * np.max(Q_table[new_state]) - Q_table[state, action])
 
-for e in range(N_EPISODES):
+    # Devia aqui alterar a tabela das transições P(state, action, new state)
+    # Devia aqui alterar a tabela das rewards para cada estado R[state]
+
+    # Q[si, a] = Q[si, a] + alpha * (r + gamma * max(Q[nsi, :]) - Q[si, a])
+    # P[a, si, nsi] += 1
+    # R[si] = r
+
+
+total_episodes = 10
+total_test_episodes = 10
+rewards = []
+
+for episode in range(total_episodes):
 
     state = start_state
-    total_reward = 0
-    alpha = alphas[e]
+    episode_rewards = []
 
-    #  for _ in range(MAX_EPISODE_STEPS):
-    parar = False
-    first = True
-    while not parar:
-        if first:
-            first = False
-        elif not first:
-            count = 0
+    while True:
 
-            for letter in next_state.grid:
-                if letter is 'b':
-                    count += 1
-            if count == 0:
-                parar = True
+        if "b" not in state.map:
+            break
 
         action = choose_action(state)
-        next_state, reward, done = act(state, action)
-        total_reward += reward
 
-        q(state)[action] = q(state, action) + \
-                           alpha * (reward + gamma * np.max(q(next_state)) - q(state, action))
-        state = next_state
-        if done:
-            break
-    # print(f"Episode {e + 1}: total reward -> {total_reward}")
+        new_state, reward = take_action(state, action)
 
-print(start_state)
-r = q(start_state)
-print(f"left={r[LEFT]}, stay={r[STAY]} ,right={r[RIGHT]}")
+        learn(state, action, reward, new_state)
 
-action_to_take = LEFT
-if r[STAY] > r[action_to_take]:
-    action_to_take = STAY
-if r[RIGHT] > r[action_to_take]:
-    action_to_take = RIGHT
+        episode_rewards.append(reward)
 
-new_state, reward, done = act(start_state, action_to_take)
+        state = new_state
 
-print(new_state)
-r = q(new_state)
-print(f"left={r[LEFT]}, stay={r[STAY]} ,right={r[RIGHT]}")
+    rewards.append(np.mean(episode_rewards))
 
-while True:
-    if "b" not in new_state.grid:
-        break
 
-    action_to_take = LEFT
-    if r[STAY] > r[action_to_take]:
-        action_to_take = STAY
-    if r[RIGHT] > r[action_to_take]:
-        action_to_take = RIGHT
-
-    new_state, reward, done = act(new_state, action_to_take)
-
-    print(new_state)
-
-    r = q(new_state)
-    print(f"left={r[LEFT]}, stay={r[STAY]} ,right={r[RIGHT]}")
+print("done")
--- oneDBoxesScenario/extra.py
+++ oneDBoxesScenario/extra.py
@@ -2,95 +2,90 @@ import numpy as np
 import random
 from copy import deepcopy
 
+
 class State:
 
-    def __init__(self, grid, car_pos):
+    def __init__(self, grid, shappy_x_pos):
         self.grid = grid
-        self.car_pos = car_pos
+        self.shappy_x_pos = shappy_x_pos
 
     def __eq__(self, other):
-        return isinstance(other, State) and self.grid == other.grid and self.car_pos == other.car_pos
+        return isinstance(other, State) and self.grid == other.grid and self.shappy_x_pos == other.shappy_x_pos
 
     def __hash__(self):
-        return hash(str(self.grid) + str(self.car_pos))
+        return hash(str(self.grid) + str(self.shappy_x_pos))
 
     def __str__(self):
-        return f"State(grid={self.grid}, car_pos={self.car_pos})"
+        return f"State(grid={self.grid}, shappy_x_pos={self.shappy_x_pos})"
 
 
-ZOMBIE = "z"
-CAR = "c"
-ICE_CREAM = "i"
+# Environment
+BOX = "b"
+COL_SHAPPY = "c"
+NON_COL_SHAPPY = "n"
 EMPTY = "*"
 
-grid = [
-    [ICE_CREAM, EMPTY],
-    [ZOMBIE, CAR]
-]
+grid = [BOX, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BOX, EMPTY, COL_SHAPPY, EMPTY, BOX]
 
-for row in grid:
-    print(' '.join(row))
+# Actions
+LEFT = 0
+STAY = 1
+RIGHT = 2
 
+ACTIONS = [LEFT, STAY, RIGHT]
 
-UP = 0
-DOWN = 1
-LEFT = 2
-RIGHT = 3
+pos = -1
 
-ACTIONS = [UP, DOWN, LEFT, RIGHT]
+for i in range(len(grid)):
+    if grid[i] == "c":
+        pos = i
 
-start_state = State(grid=grid, car_pos=[1, 1])
-
+start_state = State(grid=grid, shappy_x_pos=pos)
 
 
 def act(state, action):
-
-    def new_car_pos(state, action):
-        p = deepcopy(state.car_pos)
-        if action == UP:
-            p[0] = max(0, p[0] - 1)
-        elif action == DOWN:
-            p[0] = min(len(state.grid) - 1, p[0] + 1)
-        elif action == LEFT:
-            p[1] = max(0, p[1] - 1)
+    def new_shappy_x_pos(state, action):
+        p = deepcopy(state.shappy_x_pos)
+        if action == LEFT:
+            p = max(0, p - 1)
+        elif action == STAY:
+            p = p
         elif action == RIGHT:
-            p[1] = min(len(state.grid[0]) - 1, p[1] + 1)
+            p = min(len(grid) - 1, p + 1)
         else:
             raise ValueError(f"Unknown action {action}")
         return p
 
-    p = new_car_pos(state, action)
-    grid_item = state.grid[p[0]][p[1]]
+    p = new_shappy_x_pos(state, action)
+    grid_item = state.grid[p]
 
     new_grid = deepcopy(state.grid)
-
-    if grid_item == ZOMBIE:
-        reward = -100
+    if "b" not in grid:
         is_done = True
-        new_grid[p[0]][p[1]] += CAR
-    elif grid_item == ICE_CREAM:
-        reward = 1000
-        is_done = True
-        new_grid[p[0]][p[1]] += CAR
+    elif grid_item == BOX:
+        reward = 10
+        is_done = False
+        old = state.shappy_x_pos
+        new_grid[old] = EMPTY
+        new_grid[p] = COL_SHAPPY
     elif grid_item == EMPTY:
         reward = -1
         is_done = False
-        old = state.car_pos
-        new_grid[old[0]][old[1]] = EMPTY
-        new_grid[p[0]][p[1]] = CAR
-    elif grid_item == CAR:
+        old = state.shappy_x_pos
+        new_grid[old] = EMPTY
+        new_grid[p] = COL_SHAPPY
+    elif grid_item == COL_SHAPPY:
         reward = -1
         is_done = False
     else:
         raise ValueError(f"Unknown grid item {grid_item}")
 
-    return State(grid=new_grid, car_pos=p), reward, is_done
+    return State(grid=new_grid, shappy_x_pos=p), reward, is_done
 
 
-random.seed(42) # for reproducibility
-
-N_STATES = 4
-N_EPISODES = 5
+random.seed(42)  # for reproducibility
+N_STATES = len(grid)
+N_EPISODES = 100
 
 MAX_EPISODE_STEPS = 100
 
@@ -112,6 +107,7 @@ def q(state, action=None):
 
     return q_table[state][action]
 
+
 def choose_action(state):
     if random.uniform(0, 1) < eps:
         return random.choice(ACTIONS)
@@ -119,17 +115,31 @@ def choose_action(state):
         return np.argmax(q(state))
 
 
+#############################################
+
 for e in range(N_EPISODES):
 
     state = start_state
     total_reward = 0
     alpha = alphas[e]
 
-    for _ in range(MAX_EPISODE_STEPS):
+    #  for _ in range(MAX_EPISODE_STEPS):
+    parar = False
+    first = True
+    while not parar:
+        if first:
+            first = False
+        elif not first:
+            count = 0
+
+            for letter in state.grid:
+                if letter is 'b':
+                    count += 1
+            if count == 0:
+                parar = True
+
         action = choose_action(state)
-        print(action)
         next_state, reward, done = act(state, action)
-        print(next_state, reward, done)
         total_reward += reward
 
         q(state)[action] = q(state, action) + \
@@ -137,4 +147,37 @@ for e in range(N_EPISODES):
         state = next_state
         if done:
             break
-    print(f"Episode {e + 1}: total reward -> {total_reward}")
+    # print(f"Episode {e + 1}: total reward -> {total_reward}")
+
+print(start_state)
+r = q(start_state)
+print(f"left={r[LEFT]}, stay={r[STAY]} ,right={r[RIGHT]}")
+
+action_to_take = LEFT
+if r[STAY] > r[action_to_take]:
+    action_to_take = STAY
+if r[RIGHT] > r[action_to_take]:
+    action_to_take = RIGHT
+
+new_state, reward, done = act(start_state, action_to_take)
+
+print(new_state)
+r = q(new_state)
+print(f"left={r[LEFT]}, stay={r[STAY]} ,right={r[RIGHT]}")
+
+while True:
+    if "b" not in new_state.grid:
+        break
+
+    action_to_take = LEFT
+    if r[STAY] > r[action_to_take]:
+        action_to_take = STAY
+    if r[RIGHT] > r[action_to_take]:
+        action_to_take = RIGHT
+
+    new_state, reward, done = act(new_state, action_to_take)
+
+    print(new_state)
+
+    r = q(new_state)
+    print(f"left={r[LEFT]}, stay={r[STAY]} ,right={r[RIGHT]}")
